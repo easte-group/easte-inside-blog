467d50616134f9cd7ecd33135c5c2c86663fb1d3
kocho5252 <kocho5252@outlook.com>
2025-09-03 03:14:26 +0900

    Update members.ts

--- members.ts
+++ members.ts
@@ -2,19 +2,19 @@ import { Member } from '@src/types';
 
 export const members: Member[] = [
   {
-    id: 'kocho',
-    nickname: 'こちょ',
-    avatarSrc: '/avatars/kocho.png',
-    sources: ['https://qiita.com/kocho5252/feed'],
-    twitterUsername: 'k0cho5252',
-    githubUsername: 'kocho5252',
-  },
-  {
     id: 'koneko',
     nickname: 'koneko',
     avatarSrc: '/avatars/koneko.png',
     sources: ['https://qiita.com/saenaikoneko/feed'],
     githubUsername: 'saenaikoneko',
     websiteUrl: 'https://saenaikoneko.github.io',
+  },
+  {
+    id: 'kocho',
+    nickname: 'こちょ',
+    avatarSrc: '/avatars/kocho.png',
+    sources: ['https://qiita.com/kocho5252/feed'],
+    twitterUsername: 'k0cho5252',
+    githubUsername: 'kocho5252',
   },
 ].sort((a, b) => (a.nickname < b.nickname ? -1 : 1));
