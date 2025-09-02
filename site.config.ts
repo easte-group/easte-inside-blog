0276b2ce2dff3879d7c7b7ae39e26b4163082b8b
kocho5252 <kocho5252@outlook.com>
2025-09-03 02:30:50 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -3,6 +3,8 @@ export const config = {
     title: "easte inside blog",
     teamName: 'easte, group.',
     description: 'easte inside blogは、任意団体easteが技術的知見や団体内カルチャーを紹介するブログです。',
+    path: '/',
+    ogImage: 'og.png',
   },
   siteRoot: process.env.NODE_ENV === 'production' ? 'https://inside.easte.jp' : 'http://localhost:3000',
   headerLinks: [
