576618c088a80550a1695c62cc6c60963a522ef4
こちょ <kocho5252@outlook.com>
2025-09-22 20:44:24 +0900

    Update members.ts

--- members.ts
+++ members.ts
@@ -2,12 +2,12 @@ import { Member } from '@src/types';
 
 export const members: Member[] = [
   {
-    id: 'koneko',
-    nickname: 'koneko',
-    avatarSrc: '/avatars/koneko.png',
-    sources: ['https://qiita.com/saenaikoneko/feed'],
-    githubUsername: 'saenaikoneko',
-    websiteUrl: 'https://saenaikoneko.github.io',
+    id: 'easte',
+    nickname: 'easte',
+    avatarSrc: '/avatars/easte.png',
+    sources: ['https://qiita.com/organizations/easte/activities.atom'],
+    twitterUsername: 'easte_group',
+    websiteUrl: 'https://www.easte.jp',
   },
   {
     id: 'kocho',
@@ -17,5 +17,13 @@ export const members: Member[] = [
     twitterUsername: 'k0cho5252',
     githubUsername: 'kocho5252',
     websiteUrl: 'https://kocho5252.github.io',
+  },
+  {
+    id: 'koneko',
+    nickname: 'koneko',
+    avatarSrc: '/avatars/koneko.png',
+    sources: ['https://qiita.com/saenaikoneko/feed'],
+    githubUsername: 'saenaikoneko',
+    websiteUrl: 'https://saenaikoneko.github.io',
   },
 ].sort((a, b) => (a.nickname < b.nickname ? -1 : 1));
